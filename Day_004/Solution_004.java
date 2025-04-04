331f879c8f47948923e62f422d33ed347befe676
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-04-04 12:09:47 +0530

    Rotated an array using java

--- Day_004/Solution_004.java
+++ Day_004/Solution_004.java
@@ -4,14 +4,22 @@ class Solution {
     // Function to rotate an array by d elements in counter-clockwise direction.
     static void rotateArr(int arr[], int d) {
         int n = arr.length;
-        for (int i = 0; i < d; i++) {
-            int first = arr[0];
-            for (int j = 0; j < n - 1; j++) {
-                arr[j] = arr[j + 1];
-
-            }
-            arr[n - 1] = first;
+        d = d % n; // In the case  d > n...it will be rotated again
+        // if (d == 0) return; // No need to rotate if d is 0 or a multiple of n
+        // s1: for Reverse first d elements
+        reverse(arr, 0, d - 1);        
+        // s2: for Reverse remaining n-d elements
+        reverse(arr, d, n - 1);        
+        // s3: for Reverse the whole array
+        reverse(arr, 0, n - 1);
+    }
+    static void reverse(int[] arr, int start, int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-
     }
 }
