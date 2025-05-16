88858f3dc4bcbd2aca5434c64f193955c3a16552
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-05-16 22:48:34 +0530

    The union of two arrays with duplicates

---
+++ Day_046/Solution_046.java
@@ -0,0 +1,11 @@
+package Day_046;
+import java.util.HashSet;
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        HashSet<Integer> set =new HashSet<>();
+        for(int num1:a) set.add(num1);
+        for(int num2:b) set.add(num2);
+        return set.size();
+        
+    }
+}
