5ed186ce5f285d1d02d71a4e39d00ef5d8577be2
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-02 21:34:22 +0530

    Added Comments

--- Day_093/Solution_093.java
+++ Day_093/Solution_093.java
@@ -13,10 +13,15 @@ class Node
 public class Solution_093 {
     Node LCA(Node root, Node n1, Node n2) {
         if(root==null) return null;
+
+        // if n1 & n2 are smaller than root then go to left 
         if(root.data>n1.data && root.data>n2.data)
-            return LCA(root.left,n1,n2);
+        return LCA(root.left,n1,n2);
+
+        // if n1 & n2 are greater than root then go to right
         if(root.data<n1.data && root.data<n2.data)
             return LCA(root.right,n1,n2);
+
         return root;
     }
 }
