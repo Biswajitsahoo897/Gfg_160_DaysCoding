7e351b4e53d0c340d838dde47779d6ac3ff1090d
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-04 23:58:23 +0530

    Added Comment

--- Day_095/Solution_095.java
+++ Day_095/Solution_095.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 class Solution {
     public ArrayList<Integer> kLargest(int[] arr, int k) {
         ArrayList<Integer> res=new ArrayList<>();
-
+        //first have to sort the array then just reverse the loop 
         Arrays.sort(arr);
         
         for(int i=arr.length-1;i>=arr.length-k;i--){
